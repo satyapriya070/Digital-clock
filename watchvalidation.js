6f9ff08b72622a8544bc83e62b256329a83472ed
Satyapriya Bai <81168493+satyapriya070@users.noreply.github.com>
2021-05-18 20:15:32 +0530

    Update watchvalidation.js

--- watchvalidation.js
+++ watchvalidation.js
@@ -5,7 +5,7 @@ function DigitalClock(){
     let second=date.getSeconds();
 
     document.getElementById("template").innerHTML=
-    `${hour}: ${minutes}:${second} ${h> 12 ? "PM" : "AM"}`;
+    `${hour % 12}: ${minutes}:${second} ${h> 12 ? "PM" : "AM"}`;
 }
 
 setInterval(()=>{
